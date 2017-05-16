671a4d76350ad04b3ed1b2af3ee68c780a8ca115
Tomasz Chyc <tomek.chyc55@gmail.com>
2017-05-16 19:54:37 +0200

    create gradient in canvas and createSnake function

--- js/app.js
+++ js/app.js
@@ -7,8 +7,28 @@ $(function(){
     
     ctx.fillStyle = "#FFF";
     ctx.fillRect(0,0,$width,$height);
-    ctx.strokeStyle = '#000';
+    
+    var gradient=ctx.createLinearGradient(0,0,$width,0);
+    gradient.addColorStop("0","magenta");
+    gradient.addColorStop("0.5","blue");
+    gradient.addColorStop("1.0","red");
+    ctx.strokeStyle = gradient;
+    ctx.lineWidth = 15;
     ctx.strokeRect(0,0,$width,$height);
+    
+    
+    var $snake;
+    
+    function createSnake(){
+        var snakeLength = 4;
+        $snake = [];
+        for(var i = snakeLength - 1; i > 0; i--){
+            $snake.push({x:i,y:0});
+            console.log($snake);
+        }
+    }
+    
+    createSnake();
     
     
 });
